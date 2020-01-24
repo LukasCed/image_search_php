7ac6e8ab0c863b9d56ed37b76ca4fb69c6519fb6
Lukas Cedronas <lukascedronas1@gmail.com>
2020-01-24 05:33:20 +0200

    fix gallery tags

--- open_gallery.php
+++ open_gallery.php
@@ -10,8 +10,11 @@ function search_images_by_gallery_id() {
         echo "<p>These are the images in gallery:</p>";
 
         $link = open_connection();
-        $sql = "SELECT * FROM IMAGES.IMAGE_DATA as id
-        WHERE id.gallery_id = '$id'";
+        $sql = "SELECT *, GROUP_CONCAT(it.tag) as 'tags_concat' FROM IMAGES.IMAGE_DATA as id
+        JOIN IMAGES.IMAGE_TAG as it
+        ON it.image_id = id.id
+        WHERE id.gallery_id = '$id'
+        GROUP BY id.id";
 
         $images = mysqli_fetch_all(mysqli_query($link, $sql));
 
@@ -23,7 +26,7 @@ function search_images_by_gallery_id() {
                 $name = end($nameExploded);
                 echo "<img src='data:image/png;base64,".$base64."'/>";
                 echo "<p><b>Name:</b> $name </p>";
-                echo "<p><b>Tags:</b> $image[2] </p>";
+                echo "<p><b>Tags:</b> $image[5] </p>";
                 echo "<p><b>Gallery: </b>".get_gallery_by_id($image[2])[1]."</p>";
                 echo '<a href="edit_image.php?id='.$image[0].'"><p>Edit image</p></a>';
             }
